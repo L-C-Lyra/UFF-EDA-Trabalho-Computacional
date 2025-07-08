37b835b6c4d6c30c4bc0d5589ed5bea30ac5c281
Juliana Poustka <julianapoustka@id.uff.br>
2025-07-07 21:19:35 -0300

    reports.h

--- prototipo/reports.h
+++ prototipo/reports.h
@@ -1,13 +1,13 @@
-// Arquivo: reports.h
+
 
 #ifndef REPORTS_H
 #define REPORTS_H
 
-// Este include é necessário para a constante NAME_SIZE que usamos em reports.c
+
 #include "bplus_tree_io.h" 
 
-// Protótipo da função que queremos usar no main.c
+
 void report_calendar_grand_slam();
 
 
-#endif // REPORTS_H
+#endif 
